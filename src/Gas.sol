1a479bcf5ca58d488df34b14b8b935eb997f1547
Rastaar Haghi <rastaarhaghi@gmail.com>
2025-02-23 15:21:36 -0800

    Removed Ownable inheritance since not needed. Deployment Cost now: 2947527

--- src/Gas.sol
+++ src/Gas.sol
@@ -1,9 +1,7 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0; 
 
-import "./Ownable.sol";
-
-contract GasContract is Ownable {
+contract GasContract {
     uint256 public totalSupply; // cannot be updated
     uint256 public paymentCounter;
     mapping(address => uint256) public balances;
